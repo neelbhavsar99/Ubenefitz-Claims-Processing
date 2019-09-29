8f2301d3035226bdc10281ca98e5af4044fac23d
Neel Bhavsar <38928307+neelbhavsar99@users.noreply.github.com>
2019-09-28 20:54:37 -0400

    Update MissionStatement.js

--- amphacks/src/components/MissionStatement.js
+++ amphacks/src/components/MissionStatement.js
@@ -3,7 +3,7 @@ import '../style/mission.css';
 function MissionStatement() {
     return (
         <div className="mission">
-            <p>put mission statement text here:</p>
+            <p>How might we help Canadians take control of their health by better utilizing their benefits plan?</p>
         </div>
     );
 }
